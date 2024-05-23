56bb105a38480d2b68b2ab26f3dd8e8670f1187a
Trinetrapandey <trinetrapandey57575.tp@gmail.com>
2024-05-23 17:15:41 +0530

    final changes

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,13 +1,29 @@
 class StringCalculator
-    DELIMITERS = [",", "\n"]
-    def add(string_number)
-        return 0 if string_number.empty?
-        parse_and_sum(string_number)
-    end
+  DELIMITERS = [",", "\n"]
+  
+  def add(string_number)
+    return 0 if string_number.empty?
+    delimiters, numbers = extract_delimiters_and_numbers(string_number)
+    numbers_array = split_numbers(numbers, delimiters)
+    negatives = numbers_array.select { |num| num < 0 }
+    raise "negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
+    numbers_array.sum
+  end
 
-    private
-    
-    def parse_and_sum(string_number)
-        string_number.split(Regexp.union(DELIMITERS)).map(&:to_i).sum
+  private
+
+  def extract_delimiters_and_numbers(string_number)
+    if string_number.start_with?("//")
+      delimiter_part, numbers = string_number[2..].split("\n", 2)
+      delimiters = delimiter_part.scan(/\[([^\]]+)\]|([^\[])/).flatten.compact
+    else
+      delimiters = DELIMITERS
+      numbers = string_number
     end
+    [delimiters, numbers]
+  end
+
+  def split_numbers(numbers, delimiters)
+      numbers.split(Regexp.union(delimiters)).map(&:to_i)
+  end
 end
--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -13,13 +13,29 @@ RSpec.describe StringCalculator, type: :service do
     it "should return sum if two number are given" do
       expect(StringCalculator.new.add('1,5')).to eq(6)
     end
-    
+
     it "should return sum if multiple  number are given" do
       expect(StringCalculator.new.add('1,5,6,7,8,9,6,4,2')).to eq(48)
     end
 
     it 'handles newline charcter between string' do
       expect(StringCalculator.new.add("1\n2,3")).to eq(6)
+    end
+    
+    it 'allows custom delimiters defined at the beginning (single char)' do
+      expect(StringCalculator.new.add("//;\n1;2")).to eq(3)
+    end
+
+    it 'allows custom delimiters defined at the beginning (multiple chars)' do
+      expect(StringCalculator.new.add("//abc\n1abc2abc3")).to eq(6)
+    end
+
+    it 'allows custom delimiters of any length defined at the beginning' do
+      expect(StringCalculator.new.add("//***\n1***2***3")).to eq(6)
+    end
+
+    it 'raise exception for negative number' do
+      expect { StringCalculator.new.add("1,-2,-3") }.to raise_error("negative numbers not allowed: -2, -3")
     end
   end
 end
